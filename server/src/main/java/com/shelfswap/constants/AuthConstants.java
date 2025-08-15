ed2581f50d8a1586f63a2ce0bf33a6cd06534ab2
Tanvir Mahmud Hossain <tnvrmh41@gmail.com>
2025-08-15 23:18:40 +0600

    created utility class for constants

---
+++ server/src/main/java/com/shelfswap/constants/AuthConstants.java
@@ -0,0 +1,8 @@
+package com.shelfswap.constants;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class AuthConstants {
+    public static final long TOKEN_EXPIRATION_SECONDS = 24 * 60 * 60;
+}
