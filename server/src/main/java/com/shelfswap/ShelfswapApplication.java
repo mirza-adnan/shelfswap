6a790a315a795d545f05abab4ae8c77150267c2b
Mirza Adnan <mirza.adnan2205@gmail.com>
2025-08-15 21:18:36 +0600

    initial commit

---
+++ server/src/main/java/com/shelfswap/ShelfswapApplication.java
@@ -0,0 +1,13 @@
+package com.shelfswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfswapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfswapApplication.class, args);
+    }
+
+}
