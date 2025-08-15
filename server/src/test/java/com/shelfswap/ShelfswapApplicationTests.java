d9e795d5d1d2ba29e20e76485b26a108837c5bce
Tanvir Mahmud Hossain <tnvrmh41@gmail.com>
2025-08-15 21:55:31 +0600

    misc

---
+++ server/src/test/java/com/shelfswap/ShelfswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelfswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfswapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
