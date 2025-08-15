b0ffb8274826cd1c2db5dc42eaff9a9da5129932
Tanvir Mahmud Hossain <tnvrmh41@gmail.com>
2025-08-15 21:45:24 +0600

    added user repo, details, details service

--- server/src/main/java/com/shelfswap/entities/User.java
+++ server/src/main/java/com/shelfswap/entities/User.java
@@ -3,7 +3,6 @@ package com.shelfswap.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
---
+++ server/src/main/java/com/shelfswap/security/AppUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.shelfswap.security;
+
+import com.shelfswap.entities.User;
+import com.shelfswap.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("No user with email: " + email));
+        return new AppUserDetails(user);
+    }
+}
