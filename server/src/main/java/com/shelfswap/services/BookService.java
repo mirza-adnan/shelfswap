01075fc0cf4f9be0db641e9ccab266171fde7bb4
Mirza Adnan <mirza.adnan2205@gmail.com>
2025-08-23 19:50:58 +0600

    removed the interfaces for the services

--- server/src/main/java/com/shelfswap/services/BookService.java
+++ server/src/main/java/com/shelfswap/services/BookService.java
@@ -2,7 +2,97 @@ package com.shelfswap.services;
 
 import com.shelfswap.dtos.BookAddRequest;
 import com.shelfswap.entities.Book;
+import com.shelfswap.entities.ShelfBook;
+import com.shelfswap.entities.User;
+import com.shelfswap.entities.WishlistBook;
+import com.shelfswap.exceptions.DuplicateExistsException;
+import com.shelfswap.exceptions.NotFoundException;
+import com.shelfswap.exceptions.MutuallyExclusiveException;
+import com.shelfswap.repositories.BookRepository;
+import com.shelfswap.repositories.ShelfBookRepository;
+import com.shelfswap.repositories.WishlistRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
-public interface BookService {
-    Book addToShelfOrWishlist(BookAddRequest request, String userId, Boolean toShelf);
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BookService {
+    private final BookRepository bookRepository;
+    private final UserService userService;
+    private final ShelfBookRepository shelfBookRepository;
+    private final WishlistRepository wishlistRepository;
+
+    public Book addToShelfOrWishlist(BookAddRequest request, String userId, Boolean toShelf) {
+        Book book;
+        if (!isBookInDb(request.getId())) {
+            book = addBookToDb(request);
+            log.info("New book added to DB with title: {}", book.getTitle());
+        } else {
+            book = getBookById(request.getId());
+        }
+        User user = userService.getUserById(UUID.fromString(userId));
+
+        if (canBeAdded(user.getId(), book.getId(), toShelf)) {
+            if (toShelf) {
+                ShelfBook shelfBook = ShelfBook.builder()
+                        .user(user)
+                        .book(book)
+                        .build();
+                shelfBookRepository.save(shelfBook);
+
+                log.info("{} added to {}'s Shelf", book.getTitle(), user.getFirstName());
+            } else {
+                WishlistBook wishlistBook = WishlistBook.builder()
+                        .user(user)
+                        .book(book)
+                        .build();
+                wishlistRepository.save(wishlistBook);
+
+                log.info("{} added to {}'s Wishlist", book.getTitle(), user.getFirstName());
+            }
+        }
+
+        return book;
+    }
+
+    private Book addBookToDb(BookAddRequest request) {
+        Book book = Book.builder()
+                .id(request.getId())
+                .title(request.getTitle())
+                .author(request.getAuthor())
+                .coverUrl(String.format("https://covers.openlibrary.org/b/olid/%s-M.jpg", request.getCoverKey()))
+                .build();
+        return bookRepository.save(book);
+    }
+
+    private Boolean isBookInDb(String id) {
+        return bookRepository.existsById(id);
+    }
+
+    private Boolean canBeAdded(UUID userId, String bookId, Boolean shelf) {
+        if (shelf) {
+            if (wishlistRepository.existsByUserIdAndBookId(userId, bookId)) {
+                throw new MutuallyExclusiveException("Book is already in your wishlist. Cannot add to Shelf.");
+            }
+            if (shelfBookRepository.existsByUserIdAndBookId(userId, bookId)) {
+                throw new DuplicateExistsException("Book is already in your Shelf.");
+            }
+        } else {
+            if (shelfBookRepository.existsByUserIdAndBookId(userId, bookId)) {
+                throw new MutuallyExclusiveException("Book is already in your Shelf. Cannot add to Wishlist.");
+            }
+            if (wishlistRepository.existsByUserIdAndBookId(userId, bookId)) {
+                throw new DuplicateExistsException("Book is already in your Wishlist.");
+            }
+        }
+        return true;
+    }
+
+    public Book getBookById(String id) {
+        return bookRepository.findById(id).orElseThrow(() -> new NotFoundException("No book with given ID"));
+    }
 }
--- server/src/main/java/com/shelfswap/services/UserService.java
+++ server/src/main/java/com/shelfswap/services/UserService.java
@@ -1,10 +1,24 @@
 package com.shelfswap.services;
 
 import com.shelfswap.entities.User;
+import com.shelfswap.exceptions.UserNotFoundException;
+import com.shelfswap.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-public interface UserService {
-    User getUserById(UUID id);
-    User getUserByEmail(String email);
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User getUserById(UUID id) throws BadCredentialsException {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("No account associated with this ID"));
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("No account with email:" + email));
+    }
 }
