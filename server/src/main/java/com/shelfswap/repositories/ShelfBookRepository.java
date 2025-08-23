95768c5ce2605734c4addfd3cf112c787c945698
Tanvir Mahmud Hossain <tnvrmh41@gmail.com>
2025-08-23 19:43:48 +0600

    removed the feed logic

--- server/src/main/java/com/shelfswap/repositories/ShelfBookRepository.java
+++ server/src/main/java/com/shelfswap/repositories/ShelfBookRepository.java
@@ -12,22 +12,4 @@ import java.util.UUID;
 public interface ShelfBookRepository extends JpaRepository<ShelfBook, UUID> {
     boolean existsByUserIdAndBookId(UUID userId, String bookId);
 
-    @Query("""
-    SELECT new com.shelfswap.dtos.FeedEntryDTO(
-        new com.shelfswap.dtos.UserDTO(
-            s.user.id, s.user.email, s.user.firstName, s.user.lastName
-        ),
-        new com.shelfswap.dtos.BookDTO(
-            b.id,
-            b.title,
-            b.author,
-            b.coverUrl
-        )
-    )
-    FROM WishlistBook w
-    JOIN ShelfBook s on w.book.id = s.book.id
-    JOIN Book b on b.id = s.book.id
-    WHERE w.user.id = :userId AND s.user.id <> :userId
-""")
-    List<FeedEntryDTO> findMatchingUsersAndBooks(@Param("userId") UUID userId);
 }
