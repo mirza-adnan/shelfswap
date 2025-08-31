1b9bc63ad3ab13437003a3fefbf8e92aae11de90
Tanvir Mahmud Hossain <tnvrmh41@gmail.com>
2025-09-01 00:10:26 +0600

    added methods to get and remove books by user id an book id

--- server/src/main/java/com/shelfswap/repositories/ShelfBookRepository.java
+++ server/src/main/java/com/shelfswap/repositories/ShelfBookRepository.java
@@ -3,9 +3,11 @@ package com.shelfswap.repositories;
 import com.shelfswap.dtos.BookDTO;
 import com.shelfswap.entities.Book;
 import com.shelfswap.entities.ShelfBook;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNullApi;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,4 +22,6 @@ public interface ShelfBookRepository extends JpaRepository<ShelfBook, UUID> {
         WHERE sb.user.id = :userId
     """)
     List<Book> findShelfBooksByUserId(@Param("userId") UUID userId);
+
+    void deleteByUserIdAndBookId(UUID userId, String bookId);
 }
--- server/src/main/java/com/shelfswap/repositories/WishlistRepository.java
+++ server/src/main/java/com/shelfswap/repositories/WishlistRepository.java
@@ -1,10 +1,24 @@
 package com.shelfswap.repositories;
 
+import com.shelfswap.entities.Book;
 import com.shelfswap.entities.WishlistBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface WishlistRepository extends JpaRepository<WishlistBook, UUID> {
     boolean existsByUserIdAndBookId(UUID userId, String bookId);
+
+    @Query("""
+        SELECT b
+        FROM WishlistBook wb
+        JOIN Book b ON b.id = wb.book.id
+        WHERE wb.user.id = :userId
+    """)
+    List<Book> findWishlistBookByUserId(@Param("userId") UUID userId);
+
+    void deleteByUserIdAndBookId(UUID userId, String bookId);
 }
