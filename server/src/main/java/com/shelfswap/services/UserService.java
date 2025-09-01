1ae00b90d96928a26f808e9fcb9efb885f683a25
Mirza Adnan <mirza.adnan2205@gmail.com>
2025-09-01 19:38:35 +0600

    misc changes

--- server/src/main/java/com/shelfswap/services/UserService.java
+++ server/src/main/java/com/shelfswap/services/UserService.java
@@ -1,7 +1,9 @@
 package com.shelfswap.services;
 
+import com.shelfswap.dtos.UserDTO;
 import com.shelfswap.entities.User;
 import com.shelfswap.exceptions.UserNotFoundException;
+import com.shelfswap.mappers.UserMapper;
 import com.shelfswap.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,9 +15,15 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public User getUserById(UUID id) throws BadCredentialsException {
         return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("No account associated with this ID"));
+    }
+
+    public UserDTO getUserDtoById(UUID id) throws BadCredentialsException {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("No account associated with this ID"));
+        return userMapper.toDTO(user);
     }
 
     public User getUserByEmail(String email) {
