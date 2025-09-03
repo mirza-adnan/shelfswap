694faa144d17365ca1d46618477769a1e4da3d18
Mirza Adnan <mirza.adnan2205@gmail.com>
2025-09-03 19:27:53 +0600

    changed jwt validity

--- server/src/main/java/com/shelfswap/utils/Constants.java
+++ server/src/main/java/com/shelfswap/utils/Constants.java
@@ -7,5 +7,5 @@ import org.springframework.beans.factory.annotation.Value;
 public class Constants {
     @Value("${jwt.secret}")
     public static String secretKey;
-    public static final long TOKEN_EXPIRATION_SECONDS = 24 * 60 * 60;
+    public static final long TOKEN_EXPIRATION_SECONDS = 30 * 24 * 60 * 60;
 }
